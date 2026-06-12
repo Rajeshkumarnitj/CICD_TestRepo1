b0e5c0c0924a7fdc811f3686c0dbddc92976eb55
Rajesh Kumar <154850553+Rajeshkumarnitj@users.noreply.github.com>
2026-06-12 02:23:21 +0100

    Update Hello.java

--- NewProg.java
+++ NewProg.java
@@ -1,7 +1,7 @@
-public class NewProg
-{
-public static void main(String args[])
-{
-System.out.println("This is the program in Git");
+public class Hello{
+  public static void main(String args[]){
+    for (int i=1;i<=5;i++){
+    System.out.println("This is the program in Git");
+}
 }
 }
